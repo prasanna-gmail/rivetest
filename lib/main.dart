8e13d6d5d826557e3d9e501ee3d5b2b57d3258a4
PrasannaP <mail2lulu@gmail.com>
2023-01-19 16:23:39 +0530

    onit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:rivetest/state_machine_skills.dart';
+
+void main() => runApp(
+      MaterialApp(
+        title: 'Rive Example',
+        home: const RiveExampleApp(),
+        darkTheme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: _backgroundColor,
+          appBarTheme: const AppBarTheme(backgroundColor: _appBarColor),
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(primary: _primaryColor),
+        ),
+        themeMode: ThemeMode.dark,
+      ),
+    );
+
+/// An example application demoing Rive.
+class RiveExampleApp extends StatefulWidget {
+  const RiveExampleApp({Key? key}) : super(key: key);
+
+  @override
+  State<RiveExampleApp> createState() => _RiveExampleAppState();
+}
+
+class _RiveExampleAppState extends State<RiveExampleApp> {
+  // Example animations
+  final _pages = [
+    const _Page('Skills Machine', StateMachineSkills()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rive Examples')),
+      body: Center(
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (context, index) => _NavButton(page: _pages[index]),
+          separatorBuilder: (context, index) => const SizedBox(height: 16),
+          itemCount: _pages.length,
+        ),
+      ),
+    );
+  }
+}
+
+/// Class used to organize demo pages.
+class _Page {
+  final String name;
+  final Widget page;
+
+  const _Page(this.name, this.page);
+}
+
+/// Button to navigate to demo pages.
+class _NavButton extends StatelessWidget {
+  const _NavButton({required this.page});
+
+  final _Page page;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: SizedBox(
+          width: 250,
+          child: Center(
+            child: Text(
+              page.name,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute<void>(
+              builder: (context) => page.page,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+const _appBarColor = Color(0xFF323232);
+const _backgroundColor = Color(0xFF1D1D1D);
+const _primaryColor = Color(0xFF57A5E0);
